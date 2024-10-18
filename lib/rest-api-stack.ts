61230eb4806cf77ddcabad6d10eeae506ade77ca
ArthurBilyk02 <arthurbilyk82@gmail.com>
2024-10-18 16:39:04 +0100

    Add API endpoint for posting a new movie.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -71,10 +71,7 @@ export class RestAPIStack extends cdk.Stack {
           }),
         });
         
-        // Permissions 
-        moviesTable.grantReadData(getMovieByIdFn)
-        moviesTable.grantReadData(getAllMoviesFn)
-        
+       
         // REST API 
     const api = new apig.RestApi(this, "RestAPI", {
       description: "demo api",
@@ -89,6 +86,19 @@ export class RestAPIStack extends cdk.Stack {
       },
     });
 
+    const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/addMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
+
     const moviesEndpoint = api.root.addResource("movies");
     moviesEndpoint.addMethod(
       "GET",
@@ -100,6 +110,16 @@ export class RestAPIStack extends cdk.Stack {
       "GET",
       new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
     );
+
+    moviesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieFn, { proxy: true })
+    );
+
+
+    moviesTable.grantReadData(getMovieByIdFn)
+    moviesTable.grantReadData(getAllMoviesFn)
+    moviesTable.grantReadWriteData(newMovieFn)
         
       }
     }
